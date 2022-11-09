e1df3d3086aac9256e05c7bfbf232bc0a9987cde
Dennis Wandke <dennis.wandke@dwandke.de>
2022-11-09 23:09:24 +0100

    Copy layout from Colemad, first minor adjustments

---
+++ keyboards/crkbd/keymaps/nednisw/rules.mk
@@ -0,0 +1,22 @@
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+SPLIT_KEYBOARD = yes
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = no
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+VIA_ENABLE = no
+
+
+# Link Time Optimization (Reduces compiled size)
+LTO_ENABLE = yes
+
+SRC += ./process_records.c
+
+ifeq ($(TAP_DANCE_ENABLE),yes)
+	SRC += ./tap_dances.c
+endif
+
+ifeq ($(OLED_ENABLE),yes)
+	SRC += ./oled.c
+endif
