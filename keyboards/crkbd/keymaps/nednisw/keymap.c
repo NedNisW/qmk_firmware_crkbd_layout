7b3198942319b66f7f48ff80829cf4070386e3e1
Dennis Wandke <d.wandke@expondo.com>
2022-11-10 16:58:40 +0100

    First draft LOWER Layer

--- keyboards/crkbd/keymaps/nednisw/keymap.c
+++ keyboards/crkbd/keymaps/nednisw/keymap.c
@@ -15,21 +15,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_COLEMAK] = LAYOU
     KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSLS,
     KC_BSPC, LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_D, KC_H, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
     T_SFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC,
-    KC_LALT, KC_LCTL, LT(_RAISE, KC_SPACE), LT(_LOWER, KC_ENTER), T_SFT, KC_LGUI
+    KC_LALT, LT(_LOWER, KC_LCTL), LT(_RAISE, KC_SPACE), LT(_RAISE, KC_ENTER), LT(_LOWER, KC_RSFT), KC_LGUI
     ),
 
 [_LOWER] = LAYOUT(
-    _______, _______, KC_7, KC_8, KC_9, KC_MINS, KC_PGUP,                                      KC_HOME, KC_UP, KC_END, KC_ESC, XXXXXXX,
-    _______, KC_SLSH, KC_4, KC_5, KC_6, KC_PLUS, KC_PGDN,                                   KC_KP_4, KC_KP_5, KC_KP_6, KC_DEL, XXXXXXX,
-    _______, KC_ASTR, KC_1, KC_2, KC_3, KC_EQL,                                             XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, XXXXXXX, _______,
+    _______, KCODE_EXCL, KCODE_HASH, PARANTHESES_OPEN, PARANTHESES_CLOSE, XXXXXXX, KCODE_EURO,                                      KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH, KC_KP_ASTERISK,
+    _______, KCODE_AT, KCODE_DOLLAR, KC_LBRACKET, KC_RBRACKET, XXXXXXX, XXXXXXX,                                   KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS, KC_KP_MINUS,
+    _______, KCODE_AND, KC_NONUS_BSLASH, KCODE_UP_COMMA, KCODE_SQR, XXXXXXX,                                             0, KC_KP_1, KC_KP_2, KC_KP_3, KCODE_EQUALS, KC_KP_ENTER,
     _______, _______, _______,                                                              _______, _______, _______
     ),
 
 [_RAISE] = LAYOUT(
-    _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F11,
-    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_GRV, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_F12,
-    _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-    _______, _______, _______, _______, _______, _______
+    XXXXXXX, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F11,
+    XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_GRV, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_F12,
+    XXXXXXX, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
 [_ADJUST] = LAYOUT(
@@ -40,9 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_COLEMAK] = LAYOU
     //|--------+--------+--------+--------+--------+--------|       |--------+--------+--------+--------+--------+--------|
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|       |--------+--------+--------+--------+--------+--------|
-    XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     //                            `-------------------------'       `--------------------------'
-    ),
+    )
+};
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
--- keyboards/crkbd/keymaps/nednisw/process_records.c
+++ keyboards/crkbd/keymaps/nednisw/process_records.c
@@ -16,6 +16,90 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_LCTL(SS_LSFT("n"))); // Ctrl + Shift + N (to change keyboard layout)
             }
             return false;
+
+        case PARANTHESES_OPEN:
+            if (record->event.pressed) {
+                SEND_STRING("(");
+            }
+            return false;
+        
+        case PARANTHESES_CLOSE:
+            if (record->event.pressed) {
+                SEND_STRING(")");
+            }
+            return false;
+
+        case KCODE_EQUALS:
+            if (record->event.pressed) {
+                SEND_STRING("=");
+            }
+            return false;
+
+        case KCODE_AT:
+            if (record->event.pressed) {
+                SEND_STRING("@");
+            }
+            return false;
+
+        case KCODE_EXCL:
+            if (record->event.pressed) {
+                SEND_STRING("!");
+            }
+            return false;
+
+        case KCODE_HASH:
+            if (record->event.pressed) {
+                SEND_STRING("#");
+            }
+            return false;
+
+        case KCODE_DOLLAR:
+            if (record->event.pressed) {
+                SEND_STRING("$");
+            }
+            return false;
+
+        case KCODE_PERCENT:
+            if (record->event.pressed) {
+                SEND_STRING("%");
+            }
+            return false;
+
+        case KCODE_SQR:
+            if (record->event.pressed) {
+                SEND_STRING("^");
+            }
+            return false;
+
+        case KCODE_AND:
+            if (record->event.pressed) {
+                SEND_STRING("&");
+            }
+            return false;
+
+        case KCODE_TILDE:
+            if (record->event.pressed) {
+                SEND_STRING("~");
+            }
+            return false;
+
+        case KCODE_UP_COMMA:
+            if (record->event.pressed) {
+                SEND_STRING("`");
+            }
+            return false;
+
+        case KCODE_PIPE:
+            if (record->event.pressed) {
+                SEND_STRING("|");
+            }
+            return false;
+
+        case KCODE_SLASH:
+            if (record->event.pressed) {
+                SEND_STRING("/");
+            }
+            return false;
     }
     return true;
 }
